af48897d050904f46152bf9a4a1bbb82b9bc1ccf
Ngoc Do <nguyenanhngoc.do@gmail.com>
2017-03-05 02:59:34 -0800

    Add template SB Admin 2 & convert to Rails layout.

---
+++ app/controllers/admin/crud_controller.rb
@@ -0,0 +1,29 @@
+class Admin::CrudController < AdminController
+	def index
+		@mt = 'List CRUDs'
+	end
+
+	def show
+
+	end
+
+	def new
+
+	end
+
+	def create
+
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+end
